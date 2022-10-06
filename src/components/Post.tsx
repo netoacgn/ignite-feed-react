b5535fdaab0e81b097412bfcceb8f774e36e963a
Antonio Neto <acgomesn@gmail.com>
2022-10-06 13:25:16 -0300

    implementado javascript no projeto

---
+++ src/components/Post.tsx
@@ -0,0 +1,128 @@
+import { useState, FormEvent, ChangeEvent, InvalidEvent } from 'react';
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import { Comment } from './Comment';
+import { Avatar } from './Avatar';
+
+import styles from './Post.module.css';
+
+interface Author {
+    name: string;
+    role: string;
+    avatarUrl: string;
+}
+
+interface Content {
+    type: 'paragraph' | 'link';
+    content: string;
+}
+
+interface PostProps {
+    author: Author;
+    publishedAt: Date;
+    content: Content[];
+}
+
+export function Post({ author, publishedAt, content }: PostProps) {
+    const [comments, setComments] = useState([
+        'Post muito legal, parabéns!'
+    ]);
+    const [newCommentText, setNewCommentText] = useState('')
+
+    const publishedDateFormatted = format(publishedAt, "d' de 'LLLL' às 'HH':'mm'h'", {
+        locale: ptBR,
+    });
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+    })
+
+    function handleCreateNewComment(event: FormEvent) {
+        event.preventDefault();
+
+        setComments([...comments, newCommentText]);
+        setNewCommentText('');
+    };
+
+    function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
+        event.target.setCustomValidity('');
+        setNewCommentText(event.target.value)
+    };
+
+    function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
+        event.target.setCustomValidity('Esse campo é obrigatório');
+    };
+
+    function deleteComment(commentToDelete: string) {
+        const commentsWithoutDeletedOne = comments.filter(item => {
+            return item !== commentToDelete;
+        });
+
+        setComments(commentsWithoutDeletedOne);
+    };
+
+    const isNewCommentEmpty = newCommentText.length === 0;
+
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <Avatar src={author.avatarUrl} />
+
+                    <div className={styles.authorInfo}>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
+                    </div>
+                </div>
+
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
+            </header>
+
+            <div className={styles.content}>
+                {content.map(item => {
+                    if (item.type === 'paragraph') {
+                        return (
+                            <p key={item.content}>{item.content}</p>
+                        )
+                    } else if (item.type === 'link') {
+                        return (
+                            <p key={item.content}><a href="#">{item.content}</a></p>
+                        )
+                    }
+                })}
+            </div>
+
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea
+                    name='comment'
+                    placeholder='Deixe um comentário'
+                    onChange={handleNewCommentChange}
+                    value={newCommentText}
+                    required
+                    onInvalid={handleNewCommentInvalid}
+                />
+
+                <footer>
+                    <button type='submit' disabled={isNewCommentEmpty} >Publicar</button>
+                </footer>
+            </form>
+
+            <div className={styles.commentList}>
+                {comments.map(item => {
+                    return (
+                        <Comment
+                            key={item}
+                            content={item}
+                            onDeleteComment={deleteComment}
+                        />
+                    )
+                })}
+            </div>
+
+        </article >
+    )
+}
